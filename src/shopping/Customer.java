d2aa09bf52d30f00a2191521fd3892c32d5efef4
Gitthusiast <dorfmag@post.bgu.ac.il>
2020-11-01 12:10:34 +0200

    created Address, webuser, customer, order status, shopping cart, user state

--- src/shopping/Customer.java
+++ src/shopping/Customer.java
@@ -1,10 +1,38 @@
 package shopping;
 
+import com.sun.jndi.cosnaming.IiopUrl;
+
+import javax.jws.soap.SOAPBinding;
+
 public class Customer {
 
     private String name;
+    private String phone;
+    private String email;
+    private Address address;
+    private WebUser webUser;
 
-    public Customer(String name) {
+    public Customer(String name, String phone, String email, Address address) {
         this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.webUser = null;
+    }
+
+    public Customer(String name, String phone, String email, Address address, WebUser webUser) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.webUser = webUser;
+    }
+
+    public void addWebUser(WebUser webUser){
+
+        if(webUser == null){
+            this.webUser = webUser;
+        }
     }
 }
+
