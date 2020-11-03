9499aa41fbd5ee5ac117b24448c1f0d12d1a6c5d
yairch <50925578+yairch@users.noreply.github.com>
2020-11-03 19:04:19 +0200

    created product/supplier/LineItem

--- src/shopping/LineItem.java
+++ src/shopping/LineItem.java
@@ -7,12 +7,4 @@ public class LineItem {
     private Product product;
     private ShoppingCart shoppingCart;
     private Order order;
-
-    public LineItem(int quantity, int price, Product product, ShoppingCart shoppingCart, Order order) {
-        this.quantity = quantity;
-        this.price = price;
-        this.product = product;
-        this.shoppingCart = shoppingCart;
-        this.order = order;
-    }
 }
